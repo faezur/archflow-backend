2020bffc57c4d5c2da0d8fa25eb4ffadd6c270aa
Faiz Ansari <faiz.ansari50@gmail.com>
2026-03-16 01:20:37 +0530

    fix: add vercel URL to CORS

--- server.js
+++ server.js
@@ -12,7 +12,10 @@ const app = express();
 
 // Middleware
 app.use(cors({
-  origin: 'http://localhost:5173',
+  origin: [
+    'http://localhost:5173',
+    'https://arch-flow-mu.vercel.app'
+    ],
   credentials: true,
 }));
 app.use(express.json());
